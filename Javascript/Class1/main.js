d385f45aa0be4c02dfffa208fcbe907ae753d5a5
Abhiraj Aditya <abhiaditya860@gmail.com>
2023-12-24 19:11:07 +0530

    DOM manipulation

--- Javascript/Class1/main.js
+++ Javascript/Class1/main.js
@@ -1,24 +1,30 @@
-const jwt = require('jsonwebtoken');
-const jwtPassword = 'secret';
-const z = require('zod');
+const express = require('express');
+const app = express();
+app.use(express.json());
+const mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://abhiaditya860:ZHXUrjX1q1Rg18RV@cluster0.xnayn8w.mongodb.net/");
 
+const User = mongoose.model('Users', {name: String, email: String, password: String});
 
-const userNameSchema = z.string().email();
-const passWordSchema = z.string().min(6);
-
-function decodeJwt(token) {
-    // Your code here
-    try {
-        let decoded = jwt.decode(token, {complete: true}).header.typ;
-        if(decoded === "JWT") {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (err) {
-        return false;
-    }
-}
-const token = jwt.sign({ username: 'kirat@gmail.com', password: '123456' }, "randomPassword");
-const decoded = decodeJwt("ken");
-console.log(decoded)
+app.post('/signup', async (req, res)=>{
+  const userName = req.body.username;
+  const password = req.body.password;
+  const mail = req.body.email;
+  
+  const existingUser = await User.findOne({email: mail});
+  // CRUD --> Create, Read, Update and Delete
+  if(existingUser) {
+    return res.status(400).send("User already exists.");
+  }
+  
+  const user = new User({
+    name: userName, 
+    email: mail, 
+    password: password
+  });
+  user.save();      
+  res.json({
+    msg: "User created successfully"
+  })
+});
+app.listen(3000)
