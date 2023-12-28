cca82084fe3c307abd3a1456ade2d5d61bb6b700
Abhiraj Aditya <abhiaditya860@gmail.com>
2023-12-28 16:27:40 +0530

    message

--- assignments-master/assignments/week-3/03-mongo/routes/user.js
+++ assignments-master/assignments/week-3/03-mongo/routes/user.js
@@ -1,10 +1,14 @@
-const { Router } = require("express");
-const router = Router();
+const express = require("express");
+const router = express.Router();
 const userMiddleware = require("../middleware/user");
+const {User} = require('../db/index');
 
 // User Routes
 router.post('/signup', (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    
 });
 
 router.get('/courses', (req, res) => {
